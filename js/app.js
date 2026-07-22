e5cc3abca3646895b83a72f1c258b1b540d5f71a
Josè David Rojas Rivera <riverarojas14david@gmail.com>
2026-07-22 14:29:59 -0500

    feat: agregar proyecto página web institucional

---
+++ js/app.js
@@ -0,0 +1,3 @@
+console.log("Hola Mundo");
+console.log("David Rojas");
+console.log("Estoy aprendiendo JavaScript");
---
+++ js/ejemplo.js
@@ -0,0 +1,5 @@
+console.log("Hola Mundo");
+console.log("David Rojas");
+console.log("Bienvenido");
+console.log("Estoy aprendiendo JavaScript");
+console.log("Fin del programa");
